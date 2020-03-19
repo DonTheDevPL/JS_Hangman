48c7a2f5f14afeb482fd55878fbf66afcbb1b9a9
MichałG <DonTheDevPL@users.noreply.github.com>
2020-03-19 17:25:52 +0100

    final V 2.0

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 var alfabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","w","y","z","v","x"];
 var uzyte = new Array();
+var wynik_punktowy=0;
+function pointsHandle(typ,wynik_punktowy){
+    var wynik = document.getElementById("punkty");
+    if(typ == 0){
+        wynik_punktowy = wynik_punktowy+1;        
+        wynik.innerText="Punkty: "+ wynik_punktowy;
+        return wynik_punktowy;
+    }else{
+        if(wynik_punktowy != 0){
+        wynik_punktowy = wynik_punktowy-1;}
+        wynik.innerText="Punkty: "+ wynik_punktowy;
+        return wynik_punktowy;        
+    }
+
+}
+function zakonczGre(){
+    document.getElementById("koniec_gry").innerHTML="Poddałeś się<br>"+ "Słowo to: "+ wyraz + '<br /><span class="reset" onclick="location.reload()">Zagraj Jeszcze Raz</span>';
+    document.getElementById("literyinput").disabled = true;
+
+}
 function stworzTabele() {
     tabela = new Array();
     tabela.push("informatyka");
     tabela.push("technikum");
     tabela.push("kot");
-    tabela.push("tetrahydrokannabinol");
+    tabela.push("cyklopentanoperhydrofenantren");
     tabela.push("monitor");
     tabela.push("komputer");
     tabela.push("linux");
@@ -45,16 +65,19 @@ function handle(tab,uzyte){
         }
     }
     document.getElementById("slowo_kod").innerText=output;
-    sprawdzwygrana(tab,uzyte);
+    sprawdzwygrana(tab,uzyte,wynik_punktowy);
 }
-function sprawdzwygrana(tab,uzyte){
-    var punkty =0;
+function sprawdzwygrana(tab,uzyte,wynik){
+    var punkty = 0;
     for(var i in tab){
         if(uzyte.includes(tab[i])){
             
             punkty++;
         }
-    if(tab.length == punkty){alert("Brawo! Zgadłeś słowo");}
+        if(tab.length == punkty){
+            document.getElementById("koniec_gry").innerHTML="Gratulacje!<br> Ukończyłeś z wynikiem: "+wynik+'<br /><span class="reset" onclick="location.reload()">Zagraj Jeszcze Raz</span>';
+            document.getElementById("literyinput").disabled = true;
+        }
     }
 }
 function robListe(tag,element){
@@ -87,11 +110,13 @@ var onInput = () => {
     if (inputVal.length == 1 && alfabet.includes(inputVal) && inputVal != " " && !(uzyte.includes(inputVal))){
 
     uzyte.push(inputVal);    
-    if(tab.includes(inputVal)){
-        handle(tab,uzyte);
+    if(tab.includes(inputVal)){ 
         robListe("listaID",inputVal);
+        wynik_punktowy = pointsHandle(0,wynik_punktowy);
+        handle(tab,uzyte);        
     }else{
         robListe("listaID2",inputVal);
+        wynik_punktowy = pointsHandle(1,wynik_punktowy);
     }
 
 
